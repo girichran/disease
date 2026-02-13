ebe10252b5926835b649a11c6d7a97410304d78a
PUSHPALA GIRI CHARAN <giricharan480@gmail.com>
2026-02-13 18:12:44 +0530

    Add files via upload

---
+++ test code.py
@@ -0,0 +1,47 @@
+import numpy as np  
+import os  
+from random import shuffle  
+from tqdm import \
+    tqdm 
+import tflearn
+import tensorflow as tf
+import matplotlib.pyplot as plt
+import cv2
+import shutil
+
+
+
+
+connection = sqlite3.connect('user_data.db')
+cursor = connection.cursor()
+
+command = """CREATE TABLE IF NOT EXISTS user(name TEXT, password TEXT, mobile TEXT, email TEXT)"""
+cursor.execute(command)
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/userlog', methods=['GET', 'POST'])
+def userlog():
+    if request.method == 'POST':
+
+        connection = sqlite3.connect('user_data.db')
+        cursor = connection.cursor()
+
+        name = request.form['name']
+        password = request.form['password']
+
+        query = "SELECT name, password FROM user WHERE name = '"+name+"' AND password= '"+password+"'"
+        cursor.execute(query)
+
+        result = cursor.fetchall()
+
+        if len(result) == 0:
+            return render_template('index.html', msg='Sorry, Incorrect Credentials Provided,  Try Again')
+        else:
+            return render_template('userlog.html')
+
+    return render_template('index.html')
